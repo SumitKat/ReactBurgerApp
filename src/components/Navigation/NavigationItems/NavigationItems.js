a02c4f05c2104e2550752d299478cbfb4fec113a
Sumit Kathayat <sumitk@mindfiresolutions.com>
2021-01-18 13:23:53 +0530

    First commit

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classes from "./NavigationItems.css";
+import NavigationItem from "../NavigationItem/NavigationItem";
+
+const navigationItems = props => (
+  <ul className={classes.NavigationItems}>
+    {/* Burger Builder  Tab */}
+    <NavigationItem link="/" exact>
+      Burger Builder
+    </NavigationItem>
+
+    {/* Orders Tab */}
+    {props.isAuthenticated ? (
+      <NavigationItem link="/orders">Orders</NavigationItem>
+    ) : (
+      ""
+    )}
+
+    {/* Authenticate or Logout Tab */}
+    {!props.isAuthenticated ? (
+      <NavigationItem link="/auth">Authenticate</NavigationItem>
+    ) : (
+      <NavigationItem link="/logout">Logout</NavigationItem>
+    )}
+  </ul>
+);
+
+export default navigationItems;
